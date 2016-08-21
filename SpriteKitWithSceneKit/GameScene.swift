6aa1fb1d8820b88c974b26e1c6c2577245d9685f
Joshua B. Smith <kognate@gmail.com>
2016-08-20 23:57:32 -0600

    added particle systems for part four

--- SpriteKitWithSceneKit/GameScene.swift
+++ SpriteKitWithSceneKit/GameScene.swift
@@ -10,19 +10,25 @@ import SpriteKit
 import SceneKit
 import GameplayKit
 
+let smokeEmitter = SKEmitterNode(fileNamed: "SmokeParticles.sks")
+
 class GameScene: SKScene {
     
     var gameNode : SK3DNode = SK3DNode(viewportSize: CGSize(width: 150, height: 150.0))
-    let enemyTeapotSM = GKStateMachine(states: [Hunting(), Targeting(), Firing(), Reloading(), Destroyed()])
+    let playerNode = SK3DNode(viewportSize: CGSize(width: 100.0, height: 100.0))
+    
+    let enemyTeapotSM = GKStateMachine(states: [Hunting(), Targeting()])
     let enemyTeapotAgent = GKAgent2D()
+    let playerAgent = GKAgent2D()
     
     let etWanderGoal = GKGoal(toWander: 30)
-    let etReachSpeed = GKGoal(toReachTargetSpeed: 20.0)
+    var etSeekGoal = GKGoal()
     
     private var lastUpdateTime : TimeInterval = 0
     
     override func sceneDidLoad() {
         self.lastUpdateTime = 0
+        etSeekGoal = GKGoal(toInterceptAgent: playerAgent, maxPredictionTime: 1)
     }
     
     override func didMove(to view: SKView) {
@@ -39,16 +45,31 @@ class GameScene: SKScene {
         
         enemyTeapotSM.enter(Hunting.self)
 
-        enemyTeapotAgent.behavior = GKBehavior(goals: [etWanderGoal], andWeights: [5])
+        
+        let player = SCNScene(named: "bluebox.scn")
+
+        playerNode.name = "player"
+        //playerNode.position = CGPoint(x: self.frame.midX, y: 0)
+        playerNode.position = CGPoint(x: self.frame.midX, y: 150)
+        playerAgent.position = vector_float2(Float(playerNode.position.x), Float(playerNode.position.y))
+        playerNode.scnScene = player
+        self.addChild(playerNode)
+        
+        // cohere seems to be broken
+        let etCohereGoal = GKGoal(toCohereWith: [playerAgent], maxDistance: 10, maxAngle: Float(M_PI))
+        
+        enemyTeapotAgent.behavior = GKBehavior(goals: [etWanderGoal, etCohereGoal], andWeights: [5,10])
         enemyTeapotAgent.position = vector2(Float(teapotNode.position.x), Float(teapotNode.position.y))
-        enemyTeapotAgent.maxAcceleration = 10
-        enemyTeapotAgent.maxSpeed = 30
+        enemyTeapotAgent.maxAcceleration = 100
+        enemyTeapotAgent.maxSpeed = 100
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let firstTouch = touches.first {
             let loc = firstTouch.location(in: self)
-            gameNode.run(SKAction.move(to: loc, duration: 1))
+            playerNode.run(SKAction.move(to: loc, duration: 1))
+            playerAgent.position = vector_float2( Float(loc.x), Float(loc.y))
         }
     }
     
@@ -67,6 +88,31 @@ class GameScene: SKScene {
         
         if let enemyTeapot = self.childNode(withName: "enemy teapot") {
             enemyTeapot.position = CGPoint(x: CGFloat(enemyTeapotAgent.position.x), y: CGFloat(enemyTeapotAgent.position.y))
+
+            if (distance(enemyTeapotAgent.position, playerAgent.position) < 500) {
+                enemyTeapotSM.enter(Targeting.self)
+                enemyTeapotAgent.behavior?.setWeight(0, for: etWanderGoal)
+                enemyTeapotAgent.behavior?.setWeight(10, for: etSeekGoal)
+            } else {
+                enemyTeapotSM.enter(Hunting.self)
+                enemyTeapotAgent.behavior?.setWeight(0, for: etSeekGoal)
+                enemyTeapotAgent.behavior?.setWeight(10, for: etWanderGoal)
+            }
         }
+        
+        if let player = self.childNode(withName: "player") {
+            for node in self.nodes(at: player.position) {
+                if (node.name == "powerUp") {
+                    let puff = smokeEmitter!.copy() as! SKEmitterNode
+                    self.addChild(puff)
+                    puff.position = node.position
+                    puff.run(SKAction.sequence([SKAction.wait(forDuration: 1), SKAction.run {
+                        puff.particleBirthRate = 0
+                        }, SKAction.wait(forDuration: 1), SKAction.removeFromParent()]))
+                    node.removeFromParent()
+                }
+            }
+        }
+
     }
 }
